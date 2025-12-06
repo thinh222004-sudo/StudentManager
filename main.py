f03f0d05e4633bd64d6cac660bdb920e05f38152
thinh222004-sudo <thinh222004@gmail.com>
2025-12-06 15:20:47 +0700

    Update UI label text

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Payment.payment import Payment
 
 def main():
     user1 = User("U001", "Nguyen Dinh Thinh")
-    user2 = User("U002", "Cao Van Luan")
+    user2 = User("U002", "Cao Van Nhan")
 
     user1.show_info()
     user2.show_info()
@@ -29,3 +29,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+#aaa
